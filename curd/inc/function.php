de75c174a1bb2670200fc797058f462022acb5e2
akash_sarkar <theakashsarkar@gmail.com>
2020-08-29 06:45:12 +0600

    curd project

--- curd/inc/function.php
+++ curd/inc/function.php
@@ -1,54 +1,112 @@
-<?php 
-define('DB','/home/akash/Desktop/lwhh_source_code/curd/data/db.txt');
-function seed(){
+<?php
+define( 'DB', '/home/akash/Desktop/lwhh_source_code/curd/data/db.txt' );
+function seed() {
     $data = array(
         array(
+            'id'    => 1,
             'fname' => 'Kamal',
             'lname' => 'Ahamed',
             'roll'  => '12',
         ),
         array(
+            'id'    => 2,
             'fname' => 'Sujan',
             'lname' => 'Sarkar',
             'roll'  => '13',
         ),
         array(
+            'id'    => 3,
             'fname' => 'Nayem',
             'lname' => 'Pordan',
             'roll'  => '1',
         ),
         array(
+            'id'    => 4,
             'fname' => 'Rajon',
             'lname' => 'Sarkar',
             'roll'  => '2',
-        )
+        ),
     );
-    $serializeData = serialize($data); 
-    file_put_contents(DB,$serializeData,LOCK_EX);
+    $serializeData = serialize( $data );
+    file_put_contents( DB, $serializeData, LOCK_EX );
 };
-function genartReport(){
-    $serializeData = file_get_contents(DB);
-    $student = unserialize($serializeData);
+function genartReport() {
+    $serializeData = file_get_contents( DB );
+    $students = unserialize( $serializeData );
     ?>
-    <table>
+    <table class="table">
         <tr>
             <th>Name</th>
             <th>Roll</th>
             <th>Action</th>
         </tr>
-        <?php  
-            foreach($student as $students){
-                ?>
+        <?php foreach ( $students as $student ): ?>
                 <tr>
-                    <td><?php printf('%s %s',$students['fname'],$students['lname']);?></td>
-                    <td><?php printf('%s',$students['roll']);?></td>
-                    <td><?php printf('<a href="#">Edit</a> | <a href="#">Delet</a>');?></td>
+                    <td><?php printf( '%s %s', $student['fname'], $student['lname'] );?></td>
+                    <td><?php printf( '%s', $student['roll'] );?></td>
+                    <td><?php printf( '<a href="index.php?task=edit&id=%s">Edit</a> | <a href="index.php?task=delete&id=%s">Delete</a>', $student["id"], $student["id"] );?></td>
                 </tr>
-                <?
-                
-            }
-        ?> 
-    
+        <?php endforeach?>
     </table>
-
+    <?php
+}
+function addStudent( $fname, $lname, $roll ) {
+    $found = false;
+    $serializeData = file_get_contents( DB );
+    $students = unserialize( $serializeData );
+    foreach ( $students as $_student ) {
+        if ( $_student['roll'] == $roll ) {
+            $found = true;
+            break;
+        }
+    }
+    if ( !$found ) {
+        $newId = count( $students ) + 1;
+        $student = array(
+            'id'    => $newId,
+            'fname' => $fname,
+            'lname' => $lname,
+            'roll'  => $roll,
+        );
+        array_push( $students, $student );
+        $serializeData = serialize( $students );
+        file_put_contents( DB, $serializeData, LOCK_EX );
+        return true;
+    }
+    return false;
+}
+function getStudent( $id ) {
+    $serializeData = file_get_contents( DB );
+    $students = unserialize( $serializeData );
+    foreach ( $students as $student ) {
+        if ( $student['id'] == $id ) {
+            return $student;
+        }
+    }
+    return false;
+}
+function updateStudent( $id, $fname, $lname, $roll ) {
+    $found = false;
+    $serializeData = file_get_contents( DB );
+    $students = unserialize( $serializeData );
+    foreach ( $students as $_student ) {
+        if ( $_student['roll'] == $roll && $_student['id'] != $id ) {
+            $found = true;
+            break;
+        }
+    }
+    if ( !$found ) {
+        $students[$id - 1]['fname'] = $fname;
+        $students[$id - 1]['lname'] = $lname;
+        $students[$id - 1]['roll']  = $roll;
+        $serializeData = serialize( $students );
+        file_put_contents( DB, $serializeData, LOCK_EX );
+        return true;
+    }
+    return false;
+}
+function deleteStudent($id){
+    $serializeData = file_get_contents( DB );
+    $students      = unserialize( $serializeData );
+    unset($students['id' -1]);
 }
--- curd/index.php
+++ curd/index.php
@@ -1,10 +1,43 @@
-<?php require_once "inc/function.php"; 
-   $info = '';
-   $task = $_GET['task'] ?? 'report';
-   if('seed' == $task){
-       seed(DB);
-       $info = 'Seeding is compleate';
-   } 
+<?php require_once "inc/function.php";
+$info = '';
+$task = $_GET['task'] ?? 'report';
+$error = $_GET['error'] ?? '0';
+if('delete' == $task ){
+    $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
+    if($id > 0){
+        deleteStudent($id);
+    }
+}
+if ( 'seed' == $task ) {
+    seed( DB );
+    $info = 'Seeding is compleate';
+}
+$fname = '';
+$lname = '';
+$roll = '';
+if ( isset( $_POST['submit'] ) ) {
+    $fname = filter_input( INPUT_POST, 'fname', FILTER_SANITIZE_STRING );
+    $lname = filter_input( INPUT_POST, 'lname', FILTER_SANITIZE_STRING );
+    $roll = filter_input( INPUT_POST, 'roll', FILTER_SANITIZE_STRING );
+    $id = filter_input( INPUT_POST, 'id', FILTER_SANITIZE_STRING );
+    if ( $id ) {
+        if ( $fname != '' && $lname != '' && $roll != ''){
+            updateStudent( $id, $fname,$lname,$roll);
+            header('location:index.php?task=report');
+        }
+    } else {
+
+        if ( $fname != '' && $lname != '' && $roll != '' ) {
+            $result = addStudent( $fname, $lname, $roll );
+            if ( $result ) {
+                header( 'location:index.php?task=report' );
+            } else {
+                $error = 1;
+            }
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,22 +51,76 @@
     <div class="container">
         <div class="row">
             <div class="container-fluid">
-                <h2>project 2 -CURD</h2> 
-                <?php include_once ('inc/template/nav.php')?>
+                <h2>project 2 -CURD</h2>
+                <?php include_once 'inc/template/nav.php'?>
                 <hr/>
-                <?php 
-                    if($info != ''){
-                        echo "<p>{$info}</p>";
-                    }
-                ?>
+                <?php
+if ( $info != '' ) {
+    echo "<p>{$info}</p>";
+}
+?>
             </div>
         </div>
-        <?php if('report' == $task):?>
-        <div class="row">
-            <?php  genartReport()?>
-        </div>
-        <?php endif ?>
+        <?php if ( '1' == $error ): ?>
+            <div class="row">
+                <div class="alert alert-primary" role="alert">
+                  Duplicate Roll Number
+                </div>
+            </div>
+        <?php endif?>
+        <?php if ( 'report' == $task ): ?>
+            <div class="row">
+             <?php genartReport()?>
+            </div>
+        <?php endif?>
+        <?php if ( 'add' == $task ): ?>
+            <div class="row">
+                <form action="index.php?task=add" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputName">First Name</label>
+                        <input type="text" class="form-control" id="exampleInputName" name="fname" aria-describedby="nameHelp" placeholder="First Name" value = "<?php echo $fname ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputLast">Last Name</label>
+                        <input type="text" class="form-control" id="exampleInputLast" name="lname" placeholder="Last Name" value="<?php echo $lname ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="roll">Roll</label>
+                        <input type="number" class="form-control" id="roll" placeholder="Roll" name="roll" value="<?php echo $roll ?>">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="submit">Save</button>
+                </form>
+            </div>
+        <?php endif?>
+        <?php
+if ( 'edit' == $task ):
+    $id = filter_input( INPUT_GET, 'id', FILTER_SANITIZE_STRING );
+    $student = getStudent( $id );
+    if ( $student ):
+    ?>
+	            <div class="row">
+	                <form action="index.php?task=edit" method="POST">
+	                    <input type="hidden" value="<?php echo $id?>" name ="id">
+	                    <div class="form-group">
+	                        <label for="exampleInputName">First Name</label>
+	                        <input type="text" class="form-control" id="exampleInputName" name="fname" aria-describedby="nameHelp" placeholder="First Name" value = "<?php echo $student['fname'] ?>">
+	                    </div>
+	                    <div class="form-group">
+	                        <label for="exampleInputLast">Last Name</label>
+	                        <input type="text" class="form-control" id="exampleInputLast" name="lname" placeholder="Last Name" value="<?php echo $student['lname'] ?>">
+	                    </div>
+	                    <div class="form-group">
+	                        <label for="roll">Roll</label>
+	                        <input type="number" class="form-control" id="roll" placeholder="Roll" name="roll" value="<?php echo $student['roll'] ?>">
+	                    </div>
+	                    <button type="submit" class="btn btn-primary" name="submit">Update</button>
+	                </form>
+	            </div>
+	        <?php
+endif;
+endif;
+?>
     </div>
-        
+
 </body>
 </html>
