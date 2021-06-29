535a869607c6cc3b13b0857d480784e16726d1e9
Akash sarkar <theakashsarkar7@gmail.com>
2021-06-29 09:37:13 +0600

    error log file

--- error/e1.php
+++ error/e1.php
@@ -1,3 +1,7 @@
 <?php
-error_reporting(0);
+// error_reporting(0);
+ini_set("display_errors",0);
+ini_set("display_startup_errors",0);
+echo 12/0;
 echo $name;
+// echo get_cfg_var("cfg_file_path");
