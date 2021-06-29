33ce2255854aa03273fec527a6c8dd27c41a15bf
Akash sarkar <theakashsarkar7@gmail.com>
2021-06-29 10:00:31 +0600

    new code add

--- error/customeErrorHandling.php
+++ error/customeErrorHandling.php
@@ -1,8 +1,28 @@
 <?php
 function custom_error_handler($severity, $msg, $file, $line)
 {
-    echo "erro [{$severity}]: {$msg} in {$file} on line number";
+    switch ($severity):
+        case E_WARNING:
+            echo "erro [{$severity}]: {$msg} in {$file} on line number";
+        break;
+        case E_ERROR:
+            echo "erro [{$severity}]: {$msg} in {$file} on line number";
+        break;
+        default:
+        echo "erro [{$severity}]: {$msg} in {$file} on line number";       
+    
 }
-// set_error_handler('custom_error_handler');
+ set_error_handler('custom_error_handler');
 // trigger_error("this is an error");
 // echo substr([123], 1);
+
+function division($divident, $divisor)
+{
+    if(0 == $divisor){
+        trigger_error("cannot divied by 0");
+    }else{
+
+        return $divident/$divisor;
+    }
+}
+echo division(10,0);
